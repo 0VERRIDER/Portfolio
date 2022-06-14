c41212de57e57ee81559898c97962c0532341f63
Anshil P <anshilp@karunya.edu.in>
2022-06-15 01:50:23 +0530

    Components: moved out from pages

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Document, {Html, Head, Main, NextScript} from 'next/document'
-import loader from "./components/loading";
-import Logo from './components/logo';
+import loader from "../components/loading";
 class MyDocument extends Document {
     render() {
         return (
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styles from "../styles/Test.module.css";
-import Logo from "./components/logo";
+import Logo from "../components/logo";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";

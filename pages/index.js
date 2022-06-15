3bcdbe4db898eeda59dc7f1d84b795152bcfc3e7
Anshil P <anshilp@karunya.edu.in>
2022-06-16 01:11:35 +0530

    Fix : contact form input attributes

--- pages/index.js
+++ pages/index.js
@@ -566,7 +566,7 @@ export default function Home() {
   <input type="email" name="email" placeholder={"Enter your email"} required/>
   <div className={styles.refreshments}>
     <div>
-    <input type="radio" id="coffee" name="refreshments" value="coffee" />
+    <input type="radio" id="coffee" name="refreshments" value="coffee" checked="checked"/>
     <label htmlFor="coffee"><span className={styles.coffee}></span> Coffee</label>
     </div>
     <div>
@@ -574,7 +574,7 @@ export default function Home() {
     <label htmlFor="tea"><span className={styles.tea}></span> Tea</label>
     </div>
 </div>
-<textarea id="w3reviemessagew" name="message" rows="3" cols="50" placeholder="Message (Optional)">
+<textarea id="w3reviemessagew" name="message" rows="3" cols="50" placeholder="Message" maxLength={300} required>
   </textarea>
 <div className={styles.submitButton}>
 <button type="submit" disabled={btnState}>{btnState?"Sending...":  ( <> <span className={styles.sendIcon}></span> Send </>  )}</button>

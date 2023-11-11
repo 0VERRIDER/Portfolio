ef1b359467f282927657296e53a9aa070e2161fc
Anshil P <anshilp@karunya.edu.in>
2023-11-12 00:25:03 +0530

    Add margin bottom to logo and text in AboutMe section

--- src/app/sections/AboutMe.js
+++ src/app/sections/AboutMe.js
@@ -4,10 +4,10 @@ import LogoLined from "../resources/LogoLined";
 export default function AboutMe() {
     return(
         <Container className="flex-col md:flex-row p-10 items-center bg-black">
-                <div className="flex flex-row items-center justify-center logo py-24 md:w-1/3">
+                <div className="flex flex-row items-center justify-center logo py-24 md:w-1/3 md:mb-16">
                     <LogoLined className="md:h-24 h-32" />
                 </div>
-                <div className="text-white text-lg font-light text-justify md:w-1/2">
+                <div className="text-white text-lg font-light text-justify md:w-1/2 md:mb-16">
                     I’m a passionate <b className="font-bold">IoT Full stack developer</b> with a unique ability to 
                     seamlessly bridge the gap between <b className="font-bold">hardware and software</b>. 
                     I thrive on continuous improvement, dedicating myself to refining my skills daily. 

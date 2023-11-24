84efa29c462bf68419589c1bdcde47ce15eeec5f
Anshil <contactme@anshil.me>
2023-11-24 16:08:17 +0000

    Refactor NameTitle component

--- src/app/components/NameTitle.js
+++ src/app/components/NameTitle.js
@@ -10,9 +10,11 @@ export default function NameTitle() {
     const { isSplashScreenVisible } = useContext(SplashContext);
 
     return (
-        <div className={`flex justify-center items-center name-title ${alexandria.className} text-7xl font-bold text-center text-white uppercase antialiased h-[78px]`}>
-            <h1 className="letter">A</h1>
-            <h1 className={`line-1 ${!isSplashScreenVisible && "anim-typewriter"} w-0`} >nshil</h1>
+        <div className={`name-title ${alexandria.className} text-7xl font-bold text-center text-white uppercase antialiased h-[78px]`}>
+            <div className="flex justify-center items-center">
+                <h1 className="letter">A</h1>
+                <h1 className={`line-1 ${!isSplashScreenVisible && "anim-typewriter"} w-0`} >nshil</h1>
+            </div>
         </div>
     )
 }

78920a3239c2061d1b22d03f346a3491d7e183de
Anshil <contactme@anshil.me>
2023-11-24 17:18:08 +0000

    Refactor BaffledJobTitles component and remove unused CSS code

--- src/app/components/BaffledJobTitles.js
+++ src/app/components/BaffledJobTitles.js
@@ -2,6 +2,7 @@
 
 import baffle from "baffle";
 import { useEffect } from "react";
+import { useContext } from 'react';
 
 
 export default function BaffledJobTitles() {
@@ -16,14 +17,21 @@ export default function BaffledJobTitles() {
     ];
 
     useEffect(() => {
+        // Baffle Animation container
         let container = document.querySelector('.baffled');
+
+        // Baffle Animation configuration
         const b = baffle(container, {
             characters: characters,
             speed: 100
         });
 
         let i = 1;
-        setInterval(() => {
+        let intervalId;
+        let timeoutId;
+
+        // Baffling animation
+        const startBaffling = () => {
             b.start();
             b.text(() => jobTitles[i]);
             b.reveal(6000);
@@ -31,13 +39,35 @@ export default function BaffledJobTitles() {
             if (i === jobTitles.length) {
                 i = 0;
             }
-        }, 6000);
+        };
 
+        // Interval between each title (Except initial)
+        const startInterval = () => {
+            intervalId = setInterval(startBaffling, 6000);
+        };
+
+        // For longer initial interval
+        const startTimeout = () => {
+            timeoutId = setTimeout(() => {
+                startInterval();
+                startBaffling();
+            }, 14000);
+        };
+
+        // Start The Baffle Animation
+        startTimeout();
+
+        return () => {
+            clearInterval(intervalId);
+            clearTimeout(timeoutId);
+        };
     }, []);
 
     return (
-        <h2 className="baffled text-base font-medium tracking-widest text-opacity-60 text-center text-white uppercase antialiased w-full h-14">
-            {jobTitles[0]}
-        </h2>
+        <>
+            <h2 className="baffled text-base font-medium tracking-widest text-opacity-60 text-center text-white uppercase antialiased w-full h-14">
+                {jobTitles[0]}
+            </h2>
+        </>
     )
 }

855c1ead1605f377dd2d787ab17971439bf9f823
Anshil <contactme@anshil.me>
2023-11-29 19:33:50 +0000

    Update select-none class in BottomNav component

--- src/app/components/BottomNav.js
+++ src/app/components/BottomNav.js
@@ -7,7 +7,7 @@ export default function BottomNav(){
     const { isMenuOpen, toggleMenu } = useContext(MenuContext);
 
     return(
-        <div className="flex flex-row w-full h-20 backdrop-blur-sm z-50 fixed bottom-0 left-0 justify-between items-center px-5">
+        <div className="flex flex-row w-full h-20 backdrop-blur-sm z-50 fixed bottom-0 left-0 justify-between items-center px-5 select-none">
             <div className="flex flex-row border-white border rounded-full items-center justify-evenly w-auto px-4 h-9">
                 <span className='relative flex h-3 w-3 mr-3'>
                     <div className="absolute animate-ping inline-flex status-color bg-green-400 bg-opacity-70 rounded-full h-full w-full"></div>

655aaa9eb4b728084ce10d6cda74735389c183d9
Anshil <contactme@anshil.me>
2023-11-29 19:34:59 +0000

    Update MainHead component styles

--- src/app/sections/MainHead.js
+++ src/app/sections/MainHead.js
@@ -4,8 +4,8 @@ import NameTitle from "../components/NameTitle";
 
 export default function MainHead() {
     return (
-        <Container id="home" className="flex-col bg-gradient-to-b from-primary from-30% to-black">
-            <div className="absolute flex flex-col items-center justify-center h-5/6 w-screen">
+        <Container id="home" className="flex-col bg-gradient-to-b from-primary from-30% md:from-35% to-black">
+            <div className="absolute flex flex-col items-center justify-center h-5/6 md:h-full w-screen">
                 <div className="flex flex-col items-center justify-center">
                     <h3 className="hidden text-xl font-normal tracking-widest text-center text-white italic antialiased w-full h-9">Hello, I'm</h3>
                     <NameTitle />

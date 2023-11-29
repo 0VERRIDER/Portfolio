9a945cbc86fd8c32162e086248212a56b5245727
Anshil <contactme@anshil.me>
2023-11-29 19:34:39 +0000

    Add job titles and resume versions to strings.js

--- src/app/resources/strings.js
+++ src/app/resources/strings.js
@@ -1,5 +1,16 @@
 export const en = {
     META_TITLE: 'Anshil P',
     META_DESCRIPTION: "Its Anshil's Personal portfolio showcasing his work and skills",
+    JOB_TITLES: [
+        "Software Developer",
+        "Web Developer",
+        "Data Analyst",
+        "Fullstack Developer",
+    ],
+    RESUME_VERSIONS: [
+        ["Version 1", "v1"],
+        ["Version 2", "v2"],
+        ["Version 3", "v3"],
+    ]
 }
 

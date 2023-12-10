42fe4fbb63af88d939ea526166ae952ecfb94c4b
Anshil <contactme@anshil.me>
2023-12-10 11:32:45 +0000

    Add disable option for PWA in development environment

--- public/next.config.js
+++ public/next.config.js
@@ -4,6 +4,7 @@ const withPwa = require('next-pwa')(
         dest: 'public',
         register: true,
         skipWaiting: true,
+        disable: process.env.NODE_ENV === "development",
     })
 
 const nextConfig = {

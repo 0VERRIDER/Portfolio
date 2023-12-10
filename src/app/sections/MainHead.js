c0ba8529af2d1f02895c8b34f4ddc81f9c06bba3
Anshil <contactme@anshil.me>
2023-12-10 10:02:42 +0000

    Remove unnecessary code in MainHead component

--- src/app/sections/MainHead.js
+++ src/app/sections/MainHead.js
@@ -6,9 +6,8 @@ import CursorAnimation from "../components/titleComponents/CursorAnimation";
 export default function MainHead() {
     return (
         <Container id="home" className="flex-col bg-gradient-to-b from-primary from-30% md:from-35% to-black">
-            <div className="absolute flex flex-col items-center justify-center h-5/6 md:h-full w-screen">
+            <div className="absolute flex flex-col items-center justify-center h-5/6 w-screen">
                 <div className="flex flex-col items-center justify-center">
-                    <h3 className="hidden text-xl font-normal tracking-widest text-center text-white italic antialiased w-full h-9">Hello, I'm</h3>
                     <NameTitle />
                     <BaffledJobTitles />
                     {/* <CursorAnimation /> */}
